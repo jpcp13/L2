5344b11c9e99b6036c5ab56ff93981f769e022d1
jpcp13 <jpcp13@gmail.com>
2017-09-27 13:34:07 +0200

    added gitignore

--- listes/liste.py
+++
@@ -1,6 +0,0 @@
-with open('emails.txt', 'w') as emails_file:
-	with open('liste.txt', 'r') as students_file:
-		for line in students_file:
-			ls = line.split()
-			if ';' in ls:
-				emails_file.write(ls[-1]+',\n')
